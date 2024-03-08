353e57dcbaf7ea46be621e3a19683cd3c8aba321
MartianForest <yarleeyo@gmail.com>
2024-03-07 19:33:03 -0800

    commit to my repo

--- telegram.go
+++ telegram.go
@@ -237,7 +237,7 @@ func (r TelegramReporter) getHelp(message *tb.Message) {
 	sb.WriteString("- /status &lt;validator address&gt; - get validator missed blocks\n")
 	sb.WriteString("- /status - get the missed blocks of the validator(s) you're subscribed to\n\n")
 	sb.WriteString("Created by <a href=\"https://validator.solar\">SOLAR Labs</a> with ❤️.\n")
-	sb.WriteString("This bot is open-sourced, you can get the source code at https://github.com/solarlabsteam/missed-blocks-checker.\n\n")
+	sb.WriteString("This bot is open-sourced by YY Lee, you can get the source code at https://github.com/solarlabsteam/missed-blocks-checker.\n\n")
 	sb.WriteString("We also maintain the following tools for Cosmos ecosystem:\n")
 	sb.WriteString("- <a href=\"https://github.com/solarlabsteam/cosmos-interacter\">cosmos-interacter</a> - a bot that can return info about Cosmos-based blockchain params.\n")
 	sb.WriteString("- <a href=\"https://github.com/solarlabsteam/cosmos-exporter\">cosmos-exporter</a> - scrape the blockchain data from the local node and export it to Prometheus\n")
